9f317ad921251d1fb28a4f0b3cce5297e3e6fdec
Urvish Dobariya <urvishdobariya@syn-172-100-013-048.res.spectrum.com>
2026-06-29 08:31:21 +0530

    feat: create analytics module

---
+++ js/analytics.js
@@ -0,0 +1,21 @@
+// analytics.js
+
+export function calculateAverage(entries) {}
+
+export function calculateHighest(entries) {}
+
+export function calculateLowest(entries) {}
+
+export function calculateMedian(entries) {}
+
+export function calculateStandardDeviation(entries) {}
+
+export function calculateTimeInRange(entries) {}
+
+export function calculateHbA1c(entries) {}
+
+export function calculateGMI(entries) {}
+
+export function calculateDistribution(entries) {}
+
+export function calculateHealthScore(entries) {}
